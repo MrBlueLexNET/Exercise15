a4da9c956775683d660c468ccaafd3a177fe4353
JYM <jeanyvesmichel@yahoo.com>
2023-02-03 08:28:03 +0100

    Entities Added

---
+++ Lms.Core/Entities/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace Lms.Core.Entities
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+       public DateTime Time { get; set; }
+
+        //Foreign Keys
+        public int TournamentId { get; set; }
+
+    }
+}
---
+++ Lms.Core/Entities/Tournament.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Entities
+{
+    public class Tournament
+    {
+        public int TournamentId { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        //public string Description { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+
+        // 1/1/0001 12:00:00 AM.
+        //public DateTime EndDate { get; set; } = DateTime();
+        //UnixEpoch defines the point in time when Unix time is equal to 0.
+        //The value of this constant is equivalent to 00:00:00.0000000 UTC, January 1, 1970, in the Gregorian calendar.
+        //public static readonly DateTime UnixEpoch;
+
+        //Navigation properties
+        public ICollection<Game>? Games { get; set; } //Declare nullable
+
+    }
+}
