5eb59a62ec47f94cb45e988b6cf70a386914cfc7
JYM <jeanyvesmichel@yahoo.com>
2023-02-03 14:26:15 +0100

    TournamentRepository + Interface Added

---
+++ Lms.Core/Repositories/TournamentRepository.cs
@@ -0,0 +1,42 @@
+﻿using Lms.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Repositories
+{
+    internal class TournamentRepository : ITournamentRepository
+    {
+        public void Add(Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tournament> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Tournament>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
